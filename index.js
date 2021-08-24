46f131e0e3be8418d266c8b1315ad34b172150a8
NarendraSG <narendra@studiographene.com>
2021-08-24 15:05:48 +0000

    gitpod file changes

--- index.js
+++ index.js
@@ -12,20 +12,11 @@ App.get('/', (req, res) => {
 App.listen(3000, ()=> {
     console.log('Listening on 3000');
 })
-try{
 
-    mongoose.connect(process.env.MONGO_URI_1,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
     .then(db => {console.log("SUCCESSFULLY CONNECTED TO MONGO-1")})
     .catch(error => {
         console.log("UNSUCCESSFUL CONNECTING MONGO", error);
         throw error;
     });
 
-}catch(error){
-    mongoose.connect(process.env.MONGO_URI_2,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-    .then(db => {console.log("SUCCESSFULLY CONNECTED TO MONGO-2")})
-    .catch(error => {
-        console.log("UNSUCCESSFUL CONNECTING MONGO", error);
-        throw error;
-    });
-}
