310e947fa405a6c1de72a17ec9b22569f3b68700
NarendraSG <narendra@studiographene.com>
2021-08-24 14:49:54 +0000

    uris fix

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ App.get('/', (req, res) => {
 App.listen(3000, ()=> {
     console.log('Listening on 3000');
 })
+try{
 
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-    .then(db => {console.log("SUCCESSFULLY CONNECTED TO MONGO")})
-    .catch(error => {console.log("UNSUCCESSFUL CONNECTING MONGO", error)});
+    mongoose.connect(process.env.MONGO_URI_1,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    .then(db => {console.log("SUCCESSFULLY CONNECTED TO MONGO-1")})
+    .catch(error => {
+        console.log("UNSUCCESSFUL CONNECTING MONGO", error);
+        throw error;
+    });
+
+}catch(error){
+    mongoose.connect(process.env.MONGO_URI_2,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    .then(db => {console.log("SUCCESSFULLY CONNECTED TO MONGO-2")})
+    .catch(error => {
+        console.log("UNSUCCESSFUL CONNECTING MONGO", error);
+        throw error;
+    });
+}
